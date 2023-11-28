ed956c427576934d80c838286fcd169e91fbd4a1
markotasevski1 <markotasevski123@yahoo.com>
2023-11-28 10:45:49 +0100

    Added room

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,3 +1,15 @@
 export default function Test() {
-  return <div>test deploy</div>
+  return (
+    <div className="test-hero-wrapper">
+      <div className="header-title">Marko Tasevski</div>
+      <iframe
+        src="https://lumalabs.ai/embed/d38fda1c-f0a5-49a0-9dbf-7a933fe8827f?mode=sparkles&background=%23ffffff&color=%23000000&showTitle=true&loadBg=true&logoPosition=bottom-left&infoPosition=bottom-right&cinematicVideo=undefined&showMenu=false"
+        width="600"
+        height="500"
+        frameBorder="0"
+        title="luma embed"
+        style={{ border: 'none' }}
+      ></iframe>
+    </div>
+  )
 }
